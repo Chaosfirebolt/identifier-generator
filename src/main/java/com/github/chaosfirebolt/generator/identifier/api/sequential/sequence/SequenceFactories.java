485c797a2d60d11d7127f8fbae21cb27da526b77
Boyan Georgiev <boyan_georgiev88@abv.bg>
2023-12-02 17:05:00 +0200

    remove sequences with initial value supplier

--- src/main/java/com/github/chaosfirebolt/generator/identifier/api/sequential/sequence/SequenceFactories.java
+++ src/main/java/com/github/chaosfirebolt/generator/identifier/api/sequential/sequence/SequenceFactories.java
@@ -20,11 +20,8 @@ import com.github.chaosfirebolt.generator.identifier.api.sequential.calculation.
 import com.github.chaosfirebolt.generator.identifier.api.sequential.calculation.CalculationFactories;
 import org.apiguardian.api.API;
 
-import java.util.List;
 import java.util.Objects;
-import java.util.function.BinaryOperator;
 import java.util.function.Predicate;
-import java.util.function.Supplier;
 
 /**
  * Factory for creating different {@link Sequence}s.
@@ -32,14 +29,12 @@ import java.util.function.Supplier;
 @API(status = API.Status.STABLE, since = "2.1.0")
 public class SequenceFactories {
 
-  private static final String NO_SEQUENCES_ERROR_MESSAGE = "No sequences provided";
-
   private SequenceFactories() {
     throw new RuntimeException("No instances allowed");
   }
 
   /**
-   * Creates a finite sequence of elements from provided arguments. For the purpose of creating identifier generator, {@link #infinite}, {@link #composite} or {@link #resettable} sequences are preferred.
+   * Creates a finite sequence of elements from provided arguments. For the purpose of creating identifier generator, {@link #infinite} or {@link #resettable} sequences are preferred.
    *
    * @param initialValue the initial value of the sequence
    * @param calculation  calculates the next element, using the previous one as input
@@ -56,23 +51,6 @@ public class SequenceFactories {
   }
 
   /**
-   * Creates a finite sequence of elements from provided arguments. For the purpose of creating identifier generator, {@link #infinite}, {@link #composite} or {@link #resettable} sequences are preferred.
-   *
-   * @param initial     supplier, computing the initial value of the sequence
-   * @param calculation calculates the next element, using the previous one as input
-   * @param hasNext     condition to determine when the sequence is unable to produce more elements
-   * @param <O>         type of elements produced by this sequence
-   * @return finite sequence of elements
-   * @throws NullPointerException if any of the arguments is null
-   */
-  public static <O> Sequence<O> finite(Supplier<O> initial, Calculation<O> calculation, Predicate<O> hasNext) {
-    Objects.requireNonNull(calculation, "Null calculation passed");
-    Objects.requireNonNull(initial, "Null initial value supplier passed");
-    Calculation<O> fallbackCalculation = CalculationFactories.computableFallback(calculation, initial);
-    return new FiniteSequence<>(fallbackCalculation, Objects.requireNonNull(hasNext));
-  }
-
-  /**
    * Creates an infinite sequence of elements from provided arguments.
    *
    * @param initialValue the initial value of the sequence
@@ -85,22 +63,6 @@ public class SequenceFactories {
     Objects.requireNonNull(initialValue, "Null initial value passed");
     Objects.requireNonNull(calculation, "Null calculation passed");
     Calculation<O> fallbackCalculation = CalculationFactories.constantFallback(calculation, initialValue);
-    return new InfiniteSequence<>(fallbackCalculation);
-  }
-
-  /**
-   * Creates an infinite sequence of elements from provided arguments.
-   *
-   * @param initial     supplier, computing the initial value of the sequence
-   * @param calculation calculates the next element, using the previous one as input
-   * @param <O>         type of elements produced by this sequence
-   * @return infinite sequence of elements
-   * @throws NullPointerException if any of the arguments is null
-   */
-  public static <O> Sequence<O> infinite(Supplier<O> initial, Calculation<O> calculation) {
-    Objects.requireNonNull(initial, "Null initial value passed");
-    Objects.requireNonNull(calculation, "Null calculation passed");
-    Calculation<O> fallbackCalculation = CalculationFactories.computableFallback(calculation, initial);
     return new InfiniteSequence<>(fallbackCalculation);
   }
 
